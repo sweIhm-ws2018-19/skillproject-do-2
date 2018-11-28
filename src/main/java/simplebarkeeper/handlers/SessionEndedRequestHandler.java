8a024069a294ad57f4b14bbf90358696eb6170f0
RobinGrellner <robin.grellner@googlemail.com>
2018-11-28 14:36:52 +0100

    Javadoc Comments updated. Inline Comments with code are still included for later Use!!!

--- src/main/java/simplebarkeeper/handlers/SessionEndedRequestHandler.java
+++ src/main/java/simplebarkeeper/handlers/SessionEndedRequestHandler.java
@@ -8,7 +8,12 @@ import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.model.SessionEndedRequest;
-
+/**
+ * Handler for the SessionEndedReqeust. It is only called if the
+ * Session was ended.  Any cleanup logic is implemented here. 
+ * @author Robin Grellner, Xaver Siodlazek
+ *
+ */
 public class SessionEndedRequestHandler implements RequestHandler {
 
     @Override
@@ -16,6 +21,10 @@ public class SessionEndedRequestHandler implements RequestHandler {
         return input.matches(requestType(SessionEndedRequest.class));
     }
 
+    /**
+     * {@inheritDoc}
+     * Method That implements the cleanup logic.
+     */
     @Override
     public Optional<Response> handle(HandlerInput input) {
         return input.getResponseBuilder().build();
