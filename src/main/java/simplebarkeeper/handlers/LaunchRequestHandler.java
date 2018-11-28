432ac3565ca8a97b5c0712bb4a726643ff5bc6f3
phelyks <info@fhaala.de>
2018-11-28 08:04:42 +0100

    Codesmells removed

--- src/main/java/simplebarkeeper/handlers/LaunchRequestHandler.java
+++ src/main/java/simplebarkeeper/handlers/LaunchRequestHandler.java
@@ -2,16 +2,12 @@ package simplebarkeeper.handlers;
 
 import static com.amazon.ask.request.Predicates.requestType;
 
-import java.util.Map;
 import java.util.Optional;
 
-import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
-
-import simplebarkeeper.ListOfDrinks;
 
 public class LaunchRequestHandler implements RequestHandler {
 
@@ -24,15 +20,6 @@ public class LaunchRequestHandler implements RequestHandler {
     public Optional<Response> handle(HandlerInput input) {
         String welcome = "Hallo. Wie kann ich dir behilflich sein?";
         String repromptMessage = "Falls du Hilfe brauchst, sag einfach 'Hilf mir'";
-//        AttributesManager attributesManager = input.getAttributesManager();
-//        Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
-//
-//        if (!persistentAttributes.containsKey("firstStart")) {
-//            persistentAttributes.put("drinkList", "Hans");
-//            persistentAttributes.put("firstStart", false);
-//            attributesManager.setPersistentAttributes(persistentAttributes);
-//            attributesManager.savePersistentAttributes();
-//        }
 
         return input.getResponseBuilder()
         		.withSimpleCard("Willkommen", welcome)
