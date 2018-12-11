3deeb49258c05db2f9a378abe710c374b3277f19
phelyks <info@fhaala.de>
2018-12-11 19:58:22 +0100

    Tests updated

--- src/main/java/simplebarkeeper/ListOfDrinks.java
+++ src/main/java/simplebarkeeper/ListOfDrinks.java
@@ -3,7 +3,6 @@ package simplebarkeeper;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
